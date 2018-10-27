6bb087c0a7ed3be01eae1270165a208e12d0cf53
Alex <bethazari@gmail.com>
2018-10-27 12:15:16 +0300

    change default port to 80

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 module.exports = {
     server: {
         ip: process.env.OPENSHIFT_NODEJS_IP || "0.0.0.0",
-        port: process.env.OPENSHIFT_NODEJS_PORT || 8080
+        port: process.env.OPENSHIFT_NODEJS_PORT || 80
     },
     db: {
         // url: process.env.OPENSHIFT_MONGODB_DB_URL || "mongodb://localhost:27017/secret-santa",
